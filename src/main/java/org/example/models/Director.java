a9634429c33315b972fec7e4a8dead379f5475e3
KacperQATest <klipczynski@sii.pl>
2023-06-04 16:41:05 +0200

    final app version

--- src/main/java/org/example/models/Director.java
+++ src/main/java/org/example/models/Director.java
@@ -1,4 +1,11 @@
 package org.example.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@AllArgsConstructor
+@Data
 public class Director {
+
+    private String firstName;
+    private String lastName;
 }
